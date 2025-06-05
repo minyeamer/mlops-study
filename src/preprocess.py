fcce31dbc912f57ba2a37a569e9484eb5d386eea
minyeamer <minyeamer@gmail.com>
2025-06-06 02:12:37 +0900

    update: add data loader to preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,47 +1,80 @@
 from src.utils import read_nrows, read_csv
 
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, OrdinalEncoder
 from sklearn.model_selection import train_test_split
+import numpy as np
 import pandas as pd
 
 from typing import Literal, Optional, Sequence, Tuple
-from collections import defaultdict
 from pathlib import Path
+
+
+class Columns:
+    def __init__(self,
+            label: str,
+            drop: Optional[Sequence[str]] = None,
+            numeric: Optional[Sequence[str]] = None,
+            categorical: Optional[Sequence[str]] = None,
+            sample_data: Optional[pd.DataFrame] = None):
+        self.set_label_column(label, sample_data)
+        self.set_drop_columns(drop, sample_data)
+        self.set_numeric_columns(numeric, sample_data)
+        self.set_categorical_columns(categorical, sample_data)
+
+    def set_label_column(self, column: str, sample_data: Optional[pd.DataFrame]=None):
+        if (isinstance(sample_data, pd.DataFrame) and (column not in sample_data)) or (not column):
+            raise KeyError(str(column))
+        else: self.label = column
+
+    def set_drop_columns(self, columns: Optional[Sequence[str]]=None, sample_data: Optional[pd.DataFrame]=None):
+        if (columns is None) and isinstance(sample_data, pd.DataFrame):
+            self.drop = [__col for __col in columns if __col in sample_data]
+        else: self.drop = columns or list()
+
+    def set_numeric_columns(self, columns: Optional[Sequence[str]]=None, sample_data: Optional[pd.DataFrame]=None):
+        if (columns is None) and isinstance(sample_data, pd.DataFrame):
+            columns = sample_data.select_dtypes(include=["int64","float64"]).columns.tolist()
+        else: columns = columns or list()
+        self.numeric = [__col for __col in columns if (__col != self.label) and (__col not in self.drop)]
+
+    def set_categorical_columns(self, columns: Optional[Sequence[str]]=None, sample_data: Optional[pd.DataFrame]=None):
+        if (columns is None) and isinstance(sample_data, pd.DataFrame):
+            columns = sample_data.select_dtypes(include=["object"]).columns.tolist()
+        else: columns = columns or list()
+        self.categorical = [__col for __col in columns if (__col != self.label) and (__col not in self.drop)]
+
+
+class Options(dict):
+    def __init__(self,
+            sep: str = ',',
+            header: Optional[int] = 0,
+            encoding = "utf-8",
+            **kwargs):
+        super().__init__(sep=sep, header=header, encoding=encoding, **kwargs)
 
 
 class Dataset:
     def __init__(self,
             file_path: Path,
             label_column: str,
-            sep: str=',',
-            header: Optional[int]=0,
-            encoding="utf-8",
-            numeric_columns: Optional[Sequence[str]]=None,
-            categorical_columns: Optional[Sequence[str]]=None,
-            drop_columns: Optional[Sequence[str]]=None,
-            scaler: Optional[Literal["Standard","MinMax"]]="Standard",
-            test_size: float=0.2,
-            random_state: int=42,
+            drop_columns: Optional[Sequence[str]] = None,
+            numeric_columns: Optional[Sequence[str]] = None,
+            categorical_columns: Optional[Sequence[str]] = None,
+            scaler: Optional[Literal["Standard","MinMax"]] = "Standard",
+            test_size: float = 0.2,
+            random_state: int = 42,
+            sep: str = ',',
+            header: Optional[int] = 0,
+            encoding = "utf-8",
             **kwargs
         ):
         self.file_path = file_path
-        self.read_info = dict(sep=sep, header=header, encoding=encoding, **kwargs)
-        self._init_columns(label_column, numeric_columns, categorical_columns, drop_columns)
-        self.label_encoders = defaultdict(LabelEncoder)
+        self.options = Options(sep, header, encoding, **kwargs)
+        self.columns = Columns(label_column, drop_columns, numeric_columns, categorical_columns, sample_data=self.head())
         self._init_scaler(scaler)
+        self.encoder = OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=-1)
         self.test_size = test_size
         self.random_state = random_state
-
-    def _init_columns(self, label: str,
-                    numeric: Optional[Sequence[str]]=None,
-                    categorical: Optional[Sequence[str]]=None,
-                    drop: Optional[Sequence[str]]=None):
-        data = self.head().drop(columns=[label]+(drop or list()))
-        if numeric is None:
-            numeric = data.select_dtypes(include=["int64","float64"]).columns.tolist()
-        if categorical is None:
-            categorical = data.select_dtypes(include=["object"]).columns.tolist()
-        self.column_info = dict(label=label, numeric=numeric, categorical=categorical, drop=drop)
 
     def _init_scaler(self, scaler: Optional[Literal["Standard","MinMax"]]="Standard"):
         if scaler.lower() == "standard":
@@ -50,10 +83,10 @@ class Dataset:
             self.scaler = MinMaxScaler()
         else: self.scaler = None
 
-    def load_dataset(self) -> Tuple[pd.DataFrame,pd.DataFrame,pd.Series,pd.Series]:
+    def load_data(self) -> Tuple[pd.DataFrame,pd.DataFrame,pd.Series,pd.Series]:
         data = self[:,:]
-        X = data.drop(columns=[self.column_info["label"]])
-        y = data[self.column_info["label"]]
+        X = data.drop(columns=[self.columns.label])
+        y = data[self.columns.label]
         X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=self.test_size, random_state=self.random_state)
         X_train = self.fit_transform(X_train)
         X_val = self.transform(X_val)
@@ -67,7 +100,7 @@ class Dataset:
         return data
 
     def transform(self, data: pd.DataFrame) -> pd.DataFrame:
-        missing_cols = (set(self.column_info["numeric"]) | set(self.column_info["categorical"])) - set(data.columns)
+        missing_cols = (set(self.columns.numeric) | set(self.columns.categorical)) - set(data.columns)
         for __col in missing_cols:
             data[__col] = None
         data = self._drop_columns(data)
@@ -77,49 +110,42 @@ class Dataset:
         return data
 
     def _drop_columns(self, data: pd.DataFrame) -> pd.DataFrame:
-        if self.column_info["drop"] is not None:
-            return data.drop(columns=self.column_info["drop"])
+        if self.columns.drop:
+            return data.drop(columns=self.columns.drop)
         else: return data
 
     def _handle_missing_values(self, data: pd.DataFrame) -> pd.DataFrame:
-        for __col in self.column_info["numeric"]:
+        for __col in self.columns.numeric:
             if __col in data:
                 data[__col] = data[__col].fillna(data[__col].median())
-        for __col in self.column_info["categorical"]:
+        for __col in self.columns.categorical:
             if __col in data:
                 data[__col] = data[__col].fillna(data[__col].mode()[0])
         return data
 
     def _encode_categorical_features(self, data: pd.DataFrame, fit=True):
-        for __col in self.column_info["categorical"]:
-            if __col in data.columns:
-                if fit:
-                    data[__col] = self.label_encoders[__col].fit_transform(data[__col])
-                else:
-                    unique_values = set(data[__col].unique())
-                    known_values = set(self.label_encoders[__col].classes_)
-                    unknown_values = unique_values - known_values
-                    if unknown_values:
-                        data.loc[data[__col].isin(unknown_values), __col] = self.label_encoders[__col].classes_[0]
-                    data[__col] = self.label_encoders[__col].transform(data[__col])
+        if self.columns.categorical and (self.encoder is not None):
+            if fit:
+                data[self.columns.categorical] = self.encoder.fit_transform(data[self.columns.categorical])
+            else:
+                data[self.columns.categorical] = self.encoder.transform(data[self.columns.categorical])
         return data
 
     def _scale_numerical_features(self, data: pd.DataFrame, fit=True):
-        if self.column_info["numeric"]:
-            numerical_data = data[self.column_info["numeric"]]
+        if self.columns.numeric and (self.scaler is not None):
             if fit:
-                data[self.column_info["numeric"]] = self.scaler.fit_transform(numerical_data)
+                data[self.columns.numeric] = self.scaler.fit_transform(data[self.columns.numeric])
             else:
-                data[self.column_info["numeric"]] = self.scaler.transform(numerical_data)
+                data[self.columns.numeric] = self.scaler.transform(data[self.columns.numeric])
         return data
 
     def __len__(self) -> int:
-        return read_nrows(self.file_path, self.read_info["header"])
+        return read_nrows(self.file_path, self.options.get("header"))
 
-    def __getitem__(self, locator=slice(None)) -> pd.DataFrame:
+    def __getitem__(self, locator=None) -> pd.DataFrame:
         if isinstance(locator, Tuple) and (len(locator) <= 2):
-            return read_csv(self.file_path, *locator, **self.read_info)
-        else: return read_csv(self.file_path, locator, **self.read_info)
+            return read_csv(self.file_path, *locator, **self.options)
+        else: return read_csv(self.file_path, locator, **self.options)
 
     def head(self, n: int=5) -> pd.DataFrame:
         return self[:n]
@@ -129,4 +155,41 @@ class Dataset:
 
 
 class DataLoader:
-    ...
+    def __init__(self,
+            dataset: Dataset,
+            batch_size: int = 32,
+            shuffle: bool = True):
+        X_train, X_val, y_train, y_val = dataset.load_data()
+        self.train_dataset = (X_train, y_train)
+        self.val_dataset = (X_val, y_val)
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.toggle_dataset(split="train")
+        self._reset_indices()
+
+    def toggle_dataset(self, split: Optional[Literal["train","val"]]=None):
+        toggle = {"train": "val", "val": "train"}
+        try: self.split = split if split in toggle else toggle[self.split]
+        except: self.split = "train"
+        self.indices = np.arange(len(self.select_dataset()[1]))
+
+    def select_dataset(self) -> Tuple[pd.DataFrame,pd.Series]:
+        return self.train_dataset if self.split == "train" else self.val_dataset
+
+    def _reset_indices(self):
+        if self.shuffle:
+            np.random.shuffle(self.indices)
+        self.current = 0
+
+    def __iter__(self):
+        self._reset_indices()
+        return self
+
+    def __next__(self):
+        X, y = self.select_dataset()
+        if self.current >= len(y):
+            raise StopIteration
+        indices = self.indices[self.current:self.current+self.batch_size]
+        batch = X.iloc[indices], y.iloc[indices]
+        self.current += self.batch_size
+        return batch
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
-from typing import Optional, Sequence, Union
+from calendar import c
+from typing import Callable, List, Optional, Sequence, Union
 from pandas.errors import InvalidIndexError
 import pandas as pd
 import csv
@@ -15,16 +16,20 @@ def read_ncols(file_path: str, sep: str=',', encoding="utf-8") -> int:
             return len(file.readline().split(sep))
 
 
-def read_csv(file_path: str, rows: Union[slice,Sequence[int]]=slice(None), cols: Union[slice,Sequence[int]]=slice(None),
+def read_csv(file_path: str, rows: Optional[Union[slice,Sequence[int]]]=None, cols: Optional[Union[slice,Sequence[int]]]=None,
             sep: str=',', header: Optional[int]=0, encoding="utf-8") -> pd.DataFrame:
-    if isinstance(rows, slice):
+    if rows is None:
+        if cols is not None:
+            cols = _slice_csv_by_cols(file_path, cols, sep, encoding) if isinstance(cols, slice) else cols
+        return pd.read_csv(file_path, sep=sep, header=header, usecols=cols, encoding=encoding)
+    elif isinstance(rows, slice):
         return _read_continuous_csv_rows(file_path, rows, cols, sep, header, encoding)
     elif isinstance(rows, Sequence):
         return _read_discrete_csv_rows(file_path, rows, cols, sep, header, encoding)
     else: raise InvalidIndexError((rows, cols))
 
 
-def _read_continuous_csv_rows(file_path: str, rows: slice, cols: Union[slice,Sequence[int]],
+def _read_continuous_csv_rows(file_path: str, rows: slice, cols: Optional[Union[slice,Sequence[int]]]=None,
                             sep: str=',', header: Optional[int]=0, encoding="utf-8") -> pd.DataFrame:
     rows = _slice_csv_by_rows(file_path, rows, header)
     skiprows = range((header or 0), rows.start+(header or 0))
@@ -32,19 +37,24 @@ def _read_continuous_csv_rows(file_path: str, rows: slice, cols: Union[slice,Seq
     return pd.read_csv(file_path, sep=sep, header=header, usecols=cols, skiprows=skiprows, nrows=(rows.stop-rows.start), encoding=encoding)
 
 
-def _read_discrete_csv_rows(file_path: str, rows: Sequence[int], cols: Union[slice,Sequence[int]],
+def _read_discrete_csv_rows(file_path: str, rows: Sequence[int], cols: Optional[Union[slice,Sequence[int]]]=None,
                             sep: str=',', header: Optional[int]=0, encoding="utf-8") -> pd.DataFrame:
-    def loc(row: Sequence, cols: Union[slice,Sequence[int]]) -> Sequence:
-        if isinstance(cols, Sequence):
-            return [__cell for __i, __cell in enumerate(row) if __i in cols]
-        else: return row[cols]
+    loc = _make_discrete_locator(cols)
     with open(file_path, 'r', encoding=encoding) as file:
         rows, header = list(), None
         for __i, __row in enumerate(csv.reader(file, delimiter=sep)):
-            if __i == header: header = loc(__row, cols)
-            elif __i in rows: rows.append(loc(__row, cols))
+            if __i == header: header = loc(__row)
+            elif __i in rows: rows.append(loc(__row))
             else: pass
         return pd.DataFrame(rows, columns=header)
+
+
+def _make_discrete_locator(cols: Optional[Union[slice,Sequence[int]]]=None) -> Callable[[List[str]],List[str]]:
+    if isinstance(cols, slice):
+        return (lambda row: row[cols])
+    elif isinstance(cols, Sequence):
+        return (lambda row: [__cell for __i, __cell in enumerate(row) if __i in cols])
+    else: return (lambda row: row)
 
 
 def _slice_csv_by_rows(file_path: str, rows: slice, header: Optional[int]=0) -> range:
